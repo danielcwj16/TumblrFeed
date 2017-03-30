3050703fd3ae9140ddc603cbf05cb232668adebb
danielcwj16 <weijiec@icloud.com>
2017-03-29 22:34:42 -0400

    V1.2

---
+++ TumblrFeed/TumblrFeed/PhotosViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  TumblrFeed
+//
+//  Created by Weijie Chen on 3/29/17.
+//  Copyright © 2017 Weijie Chen. All rights reserved.
+//
+
+import UIKit
+
+var posts:[NSDictionary] = []
+
+class PhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task : URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        //print("responseDictionary: \(responseDictionary)")
+                        
+                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                        // This is how we get the 'response' field
+                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+                        
+                        // This is where you will store the returned array of posts in your posts property
+                        // self.feeds = responseFieldDictionary["posts"] as! [NSDictionary]
+                    }
+                }
+        });
+        task.resume()
+        
+        self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
+
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
